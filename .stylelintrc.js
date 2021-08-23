00f1029aab3d8b8e4b0b9de29725e57970cef8b0
nanxiaobei <nanxiaobei@gmail.com>
2021-08-24 04:07:54 +0800

    add project config

---
+++ .stylelintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-recess-order',
+    'stylelint-config-prettier'
+  ],
+  rules: {
+    'no-descending-specificity': null,
+    'custom-property-empty-line-before': null,
+    'rule-empty-line-before': ['always', { ignore: ['after-comment', 'first-nested'] }]
+  }
+};
