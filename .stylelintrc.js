fa691a0fd7415d9dd62e953b7aead2565175296a
nanxiaobei <nanxiaobei@gmail.com>
2021-12-21 14:24:46 +0800

    refine code

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -2,11 +2,11 @@ module.exports = {
   extends: [
     'stylelint-config-standard',
     'stylelint-config-recess-order',
-    'stylelint-config-prettier'
+    'stylelint-config-prettier',
   ],
   rules: {
     'no-descending-specificity': null,
     'custom-property-empty-line-before': null,
-    'rule-empty-line-before': ['always', { ignore: ['after-comment', 'first-nested'] }]
-  }
+    'rule-empty-line-before': ['always', { ignore: ['after-comment', 'first-nested'] }],
+  },
 };
