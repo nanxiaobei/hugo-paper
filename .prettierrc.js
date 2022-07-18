1880922d99fa1cb66abee50144d23d5372a87a3d
nanxiaobei <nanxiaobei@gmail.com>
2022-07-18 23:36:23 +0800

    update configs

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,3 +1,7 @@
 module.exports = {
+  printWidth: 100,
   singleQuote: true,
+
+  // https://github.com/tailwindlabs/prettier-plugin-tailwindcss
+  plugins: [require('prettier-plugin-tailwindcss')],
 };
--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,6 +1,13 @@
 module.exports = {
   extends: ['stylelint-config-one'],
   rules: {
-    // 'no-descending-specificity': null,
+    'at-rule-no-unknown': null,
+    'function-no-unknown': null,
+    'function-url-quotes': null,
+    'color-function-notation': null,
+    'no-descending-specificity': null,
+    'comment-empty-line-before': null,
+    'comment-whitespace-inside': null,
+    'custom-property-empty-line-before': null,
   },
 };
