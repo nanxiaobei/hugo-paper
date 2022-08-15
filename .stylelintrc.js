031a3e7e1cfa43a7fadfb45509fe5f550a02f011
nanxiaobei <nanxiaobei@gmail.com>
2022-08-16 00:51:52 +0800

    upgrade deps

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,7 +1,6 @@
 module.exports = {
   extends: ['stylelint-config-one'],
   rules: {
-    'at-rule-no-unknown': null,
     'function-no-unknown': null,
     'function-url-quotes': null,
     'color-function-notation': null,
