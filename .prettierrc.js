e226e84421dcefddc02be5f0ab8e293119307b49
nanxiaobei <nanxiaobei@gmail.com>
2023-08-15 04:41:40 +0800

    upgrade deps

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,5 +2,5 @@ module.exports = {
   singleQuote: true,
 
   // https://github.com/tailwindlabs/prettier-plugin-tailwindcss
-  plugins: [require('prettier-plugin-tailwindcss')],
+  plugins: ['prettier-plugin-tailwindcss'],
 };
