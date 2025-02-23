f571bde95587596ba4922c37351c1c1553986494
nanxiaobei <nanxiaobei@gmail.com>
2025-02-23 19:38:33 +0800

    tailwindcss v4

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./layouts/**/*.html'],
-  darkMode: 'class',
-  theme: {
-    extend: {},
-  },
-  plugins: [require('@tailwindcss/typography')],
-};
